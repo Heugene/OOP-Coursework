12ac4604ff1922c06bae5b05023097d07cbccab1
Yevhenii Heoka@git config --global user.name Yevhenii <109804875+Heugene@users.noreply.github.com>
2024-05-26 20:57:36 +0300

    Added discount request tests

---
+++ Discounter/Discounter/DomainTests/DiscountRequestTests.cs
@@ -0,0 +1,41 @@
+﻿using Domain;
+
+namespace DomainTests
+{
+    [TestClass]
+    public class DiscountRequestTests
+    {
+        ShopManager EmptyManager = new ShopManager();
+        Discount EmptyDiscount = new Discount();
+
+        [TestMethod]
+        public void Valid()
+        {
+            DiscountRequest request = new DiscountRequest(DateTime.Now, EmptyManager, EmptyDiscount);
+            Assert.IsTrue(request.IsValid());
+        }
+
+        [TestMethod]
+        public void CreatedDateTimeIsNull()
+        {
+            DiscountRequest request = new DiscountRequest(DateTime.Now, EmptyManager, EmptyDiscount);
+            request.CreatedDateTime = null;
+            Assert.ThrowsException<ArgumentException>(() => request.IsValid());
+        }
+
+        [TestMethod]
+        public void ManagerIsNull()
+        {
+            DiscountRequest request = new DiscountRequest(DateTime.Now, EmptyManager, EmptyDiscount);
+            request.Manager = null;
+            Assert.ThrowsException<ArgumentException>(() => request.IsValid());
+        }
+
+        public void DiscountIsNull()
+        {
+            DiscountRequest request = new DiscountRequest(DateTime.Now, EmptyManager, EmptyDiscount);
+            request.RequestedDiscount = null;
+            Assert.ThrowsException<ArgumentException>(() => request.IsValid());
+        }
+    }
+}
