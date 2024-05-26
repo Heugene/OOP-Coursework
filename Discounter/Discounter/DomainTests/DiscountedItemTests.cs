c5f599e4381f7561bfc2f5d1864c72c911156a23
Yevhenii Heoka@git config --global user.name Yevhenii <109804875+Heugene@users.noreply.github.com>
2024-05-26 20:43:49 +0300

    Added discounted item tests

---
+++ Discounter/Discounter/DomainTests/DiscountedItemTests.cs
@@ -0,0 +1,51 @@
+﻿using Domain;
+using System.Drawing;
+
+namespace DomainTests
+{
+    [TestClass]
+    public class DiscountedItemTests
+    {
+        Bitmap EmptyPicture = new Bitmap(20, 20);
+        Shop EmptyShop = new Shop();
+
+        [TestMethod]
+        public void Valid()
+        {
+            DiscountedItem discountedItem = new DiscountedItem("Item", ItemType.Product, "A regular Item", EmptyShop, EmptyPicture);
+            Assert.IsTrue(discountedItem.IsValid());
+        }
+
+        [TestMethod]
+        public void Invalid_name()
+        {
+            DiscountedItem discountedItem = new DiscountedItem("Item", ItemType.Product, "A regular Item", EmptyShop, EmptyPicture);
+            discountedItem.Name = "";
+            Assert.ThrowsException<ArgumentException>(() => discountedItem.IsValid());
+        }
+
+        [TestMethod]
+        public void Invalid_description()
+        {
+            DiscountedItem discountedItem = new DiscountedItem("Item", ItemType.Product, "A regular Item", EmptyShop, EmptyPicture);
+            discountedItem.Description = "";
+            Assert.ThrowsException<ArgumentException>(() => discountedItem.IsValid());
+        }
+
+        [TestMethod]
+        public void PictureIsNull()
+        {
+            DiscountedItem discountedItem = new DiscountedItem("Item", ItemType.Product, "A regular Item", EmptyShop, EmptyPicture);
+            discountedItem.Picture = null;
+            Assert.ThrowsException<ArgumentException>(() => discountedItem.IsValid());
+        }
+
+        [TestMethod]
+        public void ShopIsNull() 
+        {
+            DiscountedItem discountedItem = new DiscountedItem("Item", ItemType.Product, "A regular Item", EmptyShop, EmptyPicture);
+            discountedItem.Shop = null;
+            Assert.ThrowsException<ArgumentException>(() => discountedItem.IsValid());
+        }
+    }
+}
