b175b6db3138cb6181fb95b96e53e7d5a26c723d
Yevhenii Heoka@git config --global user.name Yevhenii <109804875+Heugene@users.noreply.github.com>
2024-05-26 18:07:48 +0300

    added DiscountRequest table

---
+++ Discounter/Discounter/Database/dbo/Tables/DiscountRequest.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[DiscountRequest] (
+    [Id]              INT      IDENTITY (1, 1) NOT NULL,
+    [ManagerID]       INT      NOT NULL,
+    [DiscountID]      INT      NOT NULL,
+    [CreatedDateTime] DATETIME NOT NULL,
+    [ViewedDateTime]  DATETIME NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_DiscountRequest_ToDiscount] FOREIGN KEY ([DiscountID]) REFERENCES [dbo].[Discount] ([Id]),
+    CONSTRAINT [FK_DiscountRequest_ToManager] FOREIGN KEY ([ManagerID]) REFERENCES [dbo].[ShopManager] ([Id])
+);
