681f9fec48ad302932686e1db9d1d26c8d24351c
Yevhenii Heoka@git config --global user.name Yevhenii <109804875+Heugene@users.noreply.github.com>
2024-06-02 22:14:12 +0300

    Added some controllers

---
+++ Discounter/Discounter/DAL/Common.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    internal class Common
+    {
+        internal static string ConnectionString = @"Data Source=DESKTOP-95FJVF8;Initial Catalog=DiscountDB;Integrated Security=True;Encrypt=True;TrustServerCertificate=True";
+    }
+}
