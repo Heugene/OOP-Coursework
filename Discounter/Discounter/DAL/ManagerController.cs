f8074c58c4eb93b112e95644297966773706355a
Yevhenii Heoka@git config --global user.name Yevhenii <109804875+Heugene@users.noreply.github.com>
2024-06-07 22:00:42 +0300

    Added shop manager controller

--- Discounter/Discounter/DAL/ManagerController.cs
+++ Discounter/Discounter/DAL/ManagerController.cs
@@ -46,7 +46,7 @@ namespace DAL
             }
         }
 
-        public static ShopManager GetAdmin(string login, string password)
+        public static ShopManager GetShopManager(string login, string password)
         {
             SqlConnection sqlConnection = new SqlConnection(Common.ConnectionString);
             sqlConnection.Open();
@@ -55,9 +55,76 @@ namespace DAL
             var result = cmd.ExecuteReader();
             ShopManager manager = null;
             result.Read();
-            manager = new ShopManager(Convert.ToInt32(result["Id"]), result["Name"].ToString(), Enum.Parse<UserRole>(result["Role"].ToString()), result["PhoneNumber"].ToString(), result["Email"].ToString(), "");
+            Shop shop = DAL.ShopController.GetShop(int.Parse(result["ShopID"].ToString()));
+            manager = new ShopManager(Convert.ToInt32(result["Id"]), result["Name"].ToString(), Enum.Parse<UserRole>(result["Role"].ToString()), result["PhoneNumber"].ToString(), result["Email"].ToString(), "", shop);
             sqlConnection.Close();
             return manager;
+        }
+
+
+        public static ShopManager CreateShopManager(string name, string phoneNumber, string email, string password, Shop managedShop)
+        {
+            ShopManager manager = new ShopManager(name, UserRole.ShopManager, phoneNumber, email, password, managedShop);
+            SqlConnection sqlConnection = new SqlConnection(Common.ConnectionString);
+            sqlConnection.Open();
+            SqlCommand cmd = sqlConnection.CreateCommand();
+            try
+            {
+                cmd.CommandText = $"INSERT INTO ShopManager VALUES ('{name}', '{UserRole.ShopManager}', '{email}', '{phoneNumber}', '{password}', {managedShop.ID});";
+                cmd.ExecuteNonQuery();
+            }
+            catch
+            {
+                manager = null;
+            }
+            sqlConnection.Close();
+            return manager;
+
+        }
+
+
+        public static bool UpdateShopManager(ShopManager manager, string name, string phoneNumber, string email, string password)
+        {
+            SqlConnection sqlConnection = new SqlConnection(Common.ConnectionString);
+            sqlConnection.Open();
+            SqlCommand cmd = sqlConnection.CreateCommand();
+            try
+            {
+                cmd.CommandText = $"UPDATE ShopManager SET Name = '{name}', Email = '{email}', PhoneNumber = '{phoneNumber}', Password = '{password}' WHERE Id = {manager.ID};";
+                cmd.ExecuteNonQuery();
+                sqlConnection.Close();
+                manager.Name = name;
+                manager.PhoneNumber = phoneNumber;
+                manager.Email = email;
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public static bool ChangePassword(ShopManager manager, string newPassword)
+        {
+            return UpdateShopManager(manager, manager.Name, manager.PhoneNumber, manager.Email, newPassword);
+        }
+
+        public static bool DeleteShopManager(ShopManager manager)
+        {
+            SqlConnection sqlConnection = new SqlConnection(Common.ConnectionString);
+            sqlConnection.Open();
+            SqlCommand cmd = sqlConnection.CreateCommand();
+            try
+            {
+                cmd.CommandText = $"DELETE FROM ShopManager WHERE Id = {manager.ID};";
+                cmd.ExecuteNonQuery();
+                sqlConnection.Close();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
         }
     }
 }
