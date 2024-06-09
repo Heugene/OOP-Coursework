b2bfee3c71c6287e39783fda4ef0679b7e11bcfd
Yevhenii Heoka@git config --global user.name Yevhenii <109804875+Heugene@users.noreply.github.com>
2024-06-09 18:57:35 +0300

    Reapply "Temporarely disabled the code for loading disounted item`s image"
    This reverts commit 6028fe4f01e9b99d29031a968db4fab686a4be62.

--- Discounter/Discounter/CombinedClient/DiscountListItem.cs
+++ Discounter/Discounter/CombinedClient/DiscountListItem.cs
@@ -14,6 +14,7 @@ namespace CombinedClient
     public partial class DiscountListItem : UserControl
     {
         Discount discount;
+        string currency = " грн";
 
         public Discount Discount
         {
@@ -23,10 +24,10 @@ namespace CombinedClient
                 discount = value;
                 labelDiscName.Text = "Акція: " + discount.Name;
                 labelItemName.Text = discount.Item.Name;
-                labelOldPrice.Text = "Стара ціна: " + discount.OldPrice.ToString();
-                labelNewPrice.Text = "Нова ціна: " + discount.NewPrice.ToString();
+                labelOldPrice.Text = "Стара ціна: " + discount.OldPrice.ToString("0.00") + currency;
+                labelNewPrice.Text = "Нова ціна: " + discount.NewPrice.ToString("0.00") + currency + $" (-{discount.Percentage}%)";
                 labelStart_End.Text = $"{discount.StartDateTime} - {discount.EndDateTime}";
-                labelTrademark.Text = "Торгова марка: " + discount.Item.Shop.Trademark.Name;
+                labelTrademark.Text = "ТМ: " + discount.Item.Shop.Trademark.Name;
                 pictureBox.Image = discount.Item.Picture;
             }
         }
