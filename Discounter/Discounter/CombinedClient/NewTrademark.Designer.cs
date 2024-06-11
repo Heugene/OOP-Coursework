d8698c479de42de7d8e7f462b4a7db62d53b1a04
Yevhenii Heoka@git config --global user.name Yevhenii <109804875+Heugene@users.noreply.github.com>
2024-06-12 00:08:16 +0300

    Added trademsrk creation form Added trademark update form Added trademark creation and update features to trademark CRUD form

---
+++ Discounter/Discounter/CombinedClient/NewTrademark.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace CombinedClient
+{
+    partial class NewTrademark
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            textBoxName = new TextBox();
+            textBoxDescription = new TextBox();
+            buttonCreate = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 11);
+            label1.Name = "label1";
+            label1.Size = new Size(61, 25);
+            label1.TabIndex = 0;
+            label1.Text = "Назва";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(12, 100);
+            label2.Name = "label2";
+            label2.Size = new Size(54, 25);
+            label2.TabIndex = 1;
+            label2.Text = "Опис";
+            // 
+            // textBoxName
+            // 
+            textBoxName.Location = new Point(12, 39);
+            textBoxName.Name = "textBoxName";
+            textBoxName.Size = new Size(537, 31);
+            textBoxName.TabIndex = 2;
+            // 
+            // textBoxDescription
+            // 
+            textBoxDescription.Location = new Point(12, 128);
+            textBoxDescription.Multiline = true;
+            textBoxDescription.Name = "textBoxDescription";
+            textBoxDescription.ScrollBars = ScrollBars.Vertical;
+            textBoxDescription.Size = new Size(537, 217);
+            textBoxDescription.TabIndex = 3;
+            // 
+            // buttonCreate
+            // 
+            buttonCreate.Location = new Point(229, 364);
+            buttonCreate.Name = "buttonCreate";
+            buttonCreate.Size = new Size(112, 34);
+            buttonCreate.TabIndex = 4;
+            buttonCreate.Text = "Створити";
+            buttonCreate.UseVisualStyleBackColor = true;
+            buttonCreate.Click += buttonCreate_Click;
+            // 
+            // NewTrademark
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(578, 404);
+            Controls.Add(buttonCreate);
+            Controls.Add(textBoxDescription);
+            Controls.Add(textBoxName);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            FormBorderStyle = FormBorderStyle.FixedDialog;
+            MaximizeBox = false;
+            MinimumSize = new Size(600, 400);
+            Name = "NewTrademark";
+            StartPosition = FormStartPosition.CenterParent;
+            Text = "NewTrademark";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox textBoxName;
+        private TextBox textBoxDescription;
+        private Button buttonCreate;
+    }
+}
---
+++ Discounter/Discounter/CombinedClient/NewTrademark.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CombinedClient
+{
+    public partial class NewTrademark : Form
+    {
+        public bool IsFilled { get; private set; } = false;
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+
+        public NewTrademark()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonCreate_Click(object sender, EventArgs e)
+        {
+            if (textBoxName.Text != "" && textBoxDescription.Text != "")
+            {
+                Name = textBoxName.Text;
+                Description = textBoxDescription.Text;
+                IsFilled = true;
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Не можна створити об'єкт з порожніми полями!", "Попередження", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+    }
+}
